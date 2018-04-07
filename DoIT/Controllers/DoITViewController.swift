00ca9576bb077873ade8cc270825a87e5442a18b
Jason Villegas <jvilleg@us.ibm.com@jasons-mbp.attlocal.net>
2018-04-07 15:44:36 -0500

    Updated file structure to MVC

--- DoIT/Controllers/DoITViewController.swift
+++ DoIT/Controllers/DoITViewController.swift
@@ -13,44 +13,56 @@ class DoITViewController: UITableViewController {
 	
 	let defaults = UserDefaults.standard
 	
-	var itemArray = ["Wash Car", "Find Mike", "Ride Bike"]
+	var itemArray = [Item]()
 	
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
 		
-		if let items = defaults.array(forKey: "DoITListArray") as? [String] {
+		let newItem = Item()
+		newItem.title = "Wash Car"
+		itemArray.append(newItem)
+
+		let newItem2 = Item()
+		newItem2.title = "Wash bike"
+		itemArray.append(newItem2)
+
+		let newItem3 = Item()
+		newItem3.title = "Wash house"
+		itemArray.append(newItem3)
+		itemArray.append(newItem3)
+
+		
+		if let items = defaults.array(forKey: "DoITListArray") as? [Item] {
 			itemArray = items
 		}
 		
 	}
 
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return itemArray.count
+	}
 	
 	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		
-		let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+		let cell = tableView.dequeueReusableCell(withIdentifier: "DoITCell", for: indexPath)
 		
-		cell.textLabel?.text = itemArray[indexPath.row]
+		cell.textLabel?.text = itemArray[indexPath.row].title
 		
+		
+		cell.accessoryType = itemArray[indexPath.row].done ? .checkmark : .none
+
 		return cell
-	}
-	
-	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		return itemArray.count
 	}
 	
 	//MARK - TableView Delegate Methods
 	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 		//print(itemArray[indexPath.row])
 		
+		itemArray[indexPath.row].done = !itemArray[indexPath.row].done
 		
-		if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-			tableView.cellForRow(at: indexPath)?.accessoryType = .none
-		} else {
-			tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-		}
-
+		tableView.reloadData()
 		tableView.deselectRow(at: indexPath, animated: true)
 	}
 	
@@ -64,8 +76,12 @@ class DoITViewController: UITableViewController {
 		
 		let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
 			
+			let addItem = Item()
+			
+			addItem.title = textField.text!
+			
 			//Force unwrap as it will never be empty but can later add a block that doesn't allow user to add an empty item
-			self.itemArray.append(textField.text!)
+			self.itemArray.append(addItem)
 			
 			self.defaults.set(self.itemArray, forKey: "DoITListArray")
 			
---
+++ DoIT/DataModel/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  DoIT
+//
+//  Created by Jason Villegas on 4/7/18.
+//  Copyright © 2018 Jason Villegas. All rights reserved.
+//
+
+import Foundation
+
+
+class Item {
+	var title: String = ""
+	var done: Bool = false
+}
